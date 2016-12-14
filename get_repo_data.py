9c018bbf47816b7fdbfe1f579643613e092619b8
Peter Darrow <pmdarrow@gmail.com>
2016-12-14 16:12:23 -0400

    Update repo data script to correctly fetch all FeedHenry repos.

--- get_repo_data.py
+++ get_repo_data.py
@@ -6,29 +6,31 @@ consistent with Jekyll.
 How to run:
 
 $ pip install requests
+$ pip install --pre github3.py
 $ python get_repo_data.py
 """
 
+import github3
 import json
-import requests
 
 def get_repo_data(org_name):
-    repos = requests.get('https://api.github.com/orgs/{}/repos'.format(
-        org_name)).json()
+    repos = github3.repositories_by(org_name)
     repos_dict = {}
     for r in repos:
-        repos_dict[r['name']] = {
-            'name': r['name'],
-            'description': r['description'],
-            'html_url': r['html_url'],
-            'stargazers_count': r['stargazers_count'],
-            'forks_count': r['forks_count']
+        repos_dict[r.name] = {
+            'name': r.name,
+            'description': r.description,
+            'html_url': r.html_url,
+            'stargazers_count': r.stargazers_count,
+            'forks_count': r.forks_count
         }
     return repos_dict
 
-if name == '__main__':
+if __name__ == '__main__':
     all_repos = get_repo_data('feedhenry')
     all_repos.update(get_repo_data('feedhenry-raincatcher'))
+    all_repos.update(get_repo_data('feedhenry-templates'))
+    all_repos.update(get_repo_data('aerogear'))
 
     with open('_data/feedhenry-repos.json', 'w') as f:
         json.dump(all_repos, f, indent=4)
